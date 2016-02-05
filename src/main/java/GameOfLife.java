81faff942dc78a1bd2a4ac45e3a4eb034599537f
Tim <timmetje15@hotmail.nl>
2016-02-05 15:43:18 +0100

    Pom.xml + Java + more .gitignore

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -4,5 +4,7 @@
  * No part of this plugin may be reproduced or distributed without Tim Anema's express consent
  */
 public class GameOfLife {
+    public static void main(String[] args) {
 
+    }
 }
