59c4649a600e58b1fb238ce23c7b57e84c3591ec
Tim <timmetje15@hotmail.nl>
2016-02-05 15:48:11 +0100

    Copyright

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,7 +1,6 @@
-/**
- * Copyright (c) 2016 Tim Anema
- * All Rights Reserved
- * No part of this plugin may be reproduced or distributed without Tim Anema's express consent
+/*
+ * Copyright (c) 2016 Tim & Lukas
+ * See LICENSE for license
  */
 public class GameOfLife {
     public static void main(String[] args) {
