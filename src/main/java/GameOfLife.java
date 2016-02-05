84dd53443253c38131688b5d44d581f9561e748b
Tim <timmetje15@hotmail.nl>
2016-02-05 15:32:57 +0100

    Maven

---
+++ src/main/java/GameOfLife.java
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2016 Tim Anema
+ * All Rights Reserved
+ * No part of this plugin may be reproduced or distributed without Tim Anema's express consent
+ */
+public class GameOfLife {
+
+}
