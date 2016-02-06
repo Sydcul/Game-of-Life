0acca45e59d8e1f4cc7ee8fd8717e2ff278827f5
Tim <timmetje15@hotmail.nl>
2016-02-06 20:28:22 +0100

    Worked on main frame, cell grid and events

--- src/main/java/me/thenlgamerzone/gol/GameOfLife.java
+++ src/main/java/me/thenlgamerzone/gol/GameOfLife.java
@@ -1,19 +1,59 @@
 package me.thenlgamerzone.gol;
 
+import me.thenlgamerzone.gol.cell.CellCanvas;
+
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 /*
  * Copyright (c) 2016 Tim & Lukas
  * See LICENSE for license
  */
 public class GameOfLife {
-    /**
-     * Initializes our frame and shows it
-     * @param args Arguments
-     */
-    public static void main(String[] args) {
-        GOLFrame gFrame = new GOLFrame();
+    private JFrame mainFrame;
+    private CellCanvas cellCanvas;
 
-        gFrame.setLocationRelativeTo(null);
-        gFrame.show();
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GameOfLife();
+            }
+        });
     }
 
+    /**
+     * Initializes our frame and shows it
+     */
+    public GameOfLife() {
+        // Create JFrame and JPanel
+        mainFrame = new JFrame();
+        cellCanvas = new CellCanvas(50, 50);
+
+        // Adding listeners for JPanel
+        cellCanvas.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent event) {
+                cellCanvas.mouseDrag(event);
+            }
+        });
+
+        cellCanvas.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent event) {
+                cellCanvas.resizeEvent(event);
+            }
+        });
+
+        // Configuring JFrame
+        mainFrame.getContentPane().add(cellCanvas);
+        mainFrame.setSize(500, 500);
+        mainFrame.setTitle("Game of Life");
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setLocationRelativeTo(null);
+
+        // Show the JFrame
+        mainFrame.setVisible(true);
+    }
 }
--- src/main/java/me/thenlgamerzone/gol/cell/CellCanvas.java
+++ src/main/java/me/thenlgamerzone/gol/cell/CellCanvas.java
@@ -2,47 +2,87 @@ package me.thenlgamerzone.gol.cell;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 /*
  * Copyright (c) 2016 Tim & Lukas
  * See LICENSE for license
  */
-public class CellCanvas extends Canvas {
+public class CellCanvas extends JPanel {
     // Used double-buffer so we don't draw directly on JPanel thus preventing flickering
-    private Image drawImage;
+    private BufferedImage drawImage;
     private Graphics drawGraphics;
 
-    private final short width;
-    private final short height;
+    private final int width;
+    private final int height;
 
-    public CellCanvas(short width, short height, JPanel jPanel) {
+    public CellCanvas(int width, int height) {
         this.width = width;
         this.height = height;
     }
+    public void paint(Graphics g) {
+        super.paintComponent(g);
 
-    /**
-     * Redraws the canvas
-     */
-    private void redraw() {
+        // Init drawImage and drawGraphics
+        if(drawImage == null)
+            drawImage = (BufferedImage) createImage(getWidth(), getHeight());
+        drawGraphics = drawImage.getGraphics();
+
         // Create the background of the canvas
         drawGraphics.setColor(Color.DARK_GRAY);
-        drawGraphics.fillRect(0 ,0,getWidth(), getHeight());
+        drawGraphics.fillRect(0, 0, getWidth(), getHeight());
+
+        // Loop through height and width to create lines which are equally separated
+        // Horizontal lines
+        drawGraphics.setColor(Color.BLACK);
+        for(int horLines = 0; horLines < height; horLines++) {
+            int yCoord = horLines * getHeight() / height;
+
+            // Draw the line
+            drawGraphics.drawLine(0, yCoord, getWidth(), yCoord);
+        }
+
+        // Vertical lines
+        for (int vertLines = 0; vertLines < width; vertLines++) {
+            int xCoord = vertLines * getWidth() / width;
+
+            // Draw the line
+            drawGraphics.drawLine(xCoord, 0, xCoord, getHeight());
+        }
+
+        // TODO: Color alive cells
+        // Update the main canvas
+        g.drawImage(drawImage, 0, 0, this);
     }
 
     /**
      * Listener for mouse drags
+     *
      * @param event The event
      */
-    private void mouseDrag(MouseEvent event) {
+    public void mouseDrag(MouseEvent event) {
         // Calculate coordinate
         int x = width * event.getX() / getWidth();
         int y = height * event.getY() / getHeight();
 
         // Check whether the user is actually clicking with left
-        if(SwingUtilities.isLeftMouseButton(event)){
+        if (SwingUtilities.isLeftMouseButton(event)) {
             // TODO: activate cell
-            redraw();
+            paint(getGraphics());
         }
+    }
+
+    /**
+     * Listener for resize events, due to the double buffering a new image and graphics are needed
+     * @param evt The event
+     */
+    public void resizeEvent(ComponentEvent evt) {
+        drawImage = (BufferedImage) createImage(getWidth(), getHeight());
+        drawGraphics = drawImage.getGraphics();
+
+        // Redraw the canvas
+        paint(getGraphics());
     }
 }
